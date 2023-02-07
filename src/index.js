a728ae7076783eb66b46b081e91ac00e9b33d4de
Syed Raza <syed.raza011@gmail.com>
2023-02-07 10:34:09 -0500

    added router wrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import { store } from './state/store';
@@ -11,10 +12,11 @@ import './styles/index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-  
+  <Router>
       <React.StrictMode>
         <App />
       </React.StrictMode>
+      </Router>
   </Provider>
 );
 
